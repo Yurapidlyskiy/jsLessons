22feaeabd8c6130e7d4dc0a5fc2741baf2bf6911
Yurapidlyskiy <68945119+Yurapidlyskiy@users.noreply.github.com>
2020-12-04 22:46:40 +0200

    homework6

---
+++ homework6/index.js
@@ -0,0 +1,49 @@
+// звд 1
+
+// const keys = Object.keys(user);
+
+// for (let key of keys) {
+//     console.log(`${key}: ${user[key]}`);
+// }
+
+// const user = {
+//     name: 'Mango',
+//     age: 20,
+//     hobby: 'html',
+//     premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+// console.log(user);
+
+
+// звд 4
+
+// const countTotalSalary = function (employees) {
+//     let total = 0;
+//     const values = Object.values(employees);
+//     for (let value of values) {
+//         total += value;
+//     }
+//     return total;
+// };
+  
+// console.log(countTotalSalary({})); // 0
+  
+// console.log(
+//     countTotalSalary({
+//       mango: 100,
+//       poly: 150,
+//       alfred: 80,
+//     }),
+// ); // 330
+  
+// console.log(
+//     countTotalSalary({
+//       kiwi: 200,
+//       lux: 50,
+//       chelsy: 150,
+//     }),
+// ); // 400
